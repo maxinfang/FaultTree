5cb95ef6f3ac747e5fd8fd80b09786c1f23f7818
Ma Xinfang <maxinfang@gmail.com>
2018-09-24 14:57:57 +1000

    update the color for sub

--- shape.js
+++ shape.js
@@ -56,7 +56,7 @@ function addDiamond(dragzone,color) {
  
 function addRect(dragzone,color) { 
     
-     
+      if(color==null) color= "#0060bf";
    var paper = new Raphael(
         $(dragzone).get(0), 100,150); 
    // paper.path("M20,150 Q50,5 80,150Z") ;
