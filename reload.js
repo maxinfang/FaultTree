bf938a6b69e6e7c30c8d6279b85f4f233f5c4a56
Ma Xinfang <maxinfang@gmail.com>
2018-11-30 12:18:23 +1100

    fix the initial landing

--- reload.js
+++ reload.js
@@ -77,13 +77,19 @@ function redraw(history){
       
      if (myNodes == []) return;
        
+       
      if(mode != "submission"){
     for(n=0; n<myNodes.length;n++){ 
-         var node= myNodes[n];
+      myNodes=deserialise(history);
+        var node= myNodes[n];
        console.log(node);
          drawnode(node);
        } 
-    if(mode!="submission") {sentToparentPage();}
+       
+       
+    if(mode!="submission") {
+      
+      sentToparentPage();}
   
 }
   
@@ -91,9 +97,9 @@ function redraw(history){
   
     if(mode =="submission"){
       
-      var correctAnswer= getCorrectAnswer();
- var _history= getsubmission();
-var tolerance_prob=getToleranceprob();
+  var correctAnswer= getCorrectAnswer();
+  var _history= getsubmission();
+  var tolerance_prob=getToleranceprob();
 
       
 function getCorrectAnswer(){
